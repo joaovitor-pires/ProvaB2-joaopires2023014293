eabd911dcca5e2e712f0936efd6f3a5332af8b49
joaovitor-pires <joao.2023014293@alunocesul.com>
2025-07-01 21:47:32 -0300

    criado arquivo create-book.controller.ts

---
+++ src/Book/create-book-controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { CreateBookService } from "src/Book/create-book.service";
+
+const createBookBodySchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  publicationYear: z.number(),
+  isbn: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createBookBodySchema);
+
+type CreateBookBodySchema = z.infer<typeof createBookBodySchema>;
+
+@Controller('/books')
+export class CreateProductController {
+  constructor(private createBook: CreateBookService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body(bodyValidationPipe) body: CreateBookBodySchema) {
+    const {
+        title,
+        author,
+        publicationYear,
+        isbn,
+    } = body;
+
+    await this.createBook.execute({
+        title,
+        author,
+        publicationYear,
+        isbn,
+    });
+  }
+}
